a46a8d6cf062571f89e812601741a8401aa1162e
Martin Mihura <mbmihura@gmail.com>
2013-09-09 16:00:10 -0300

    Adds initial structure, class simulation with constructor() and Run() methods.

---
+++ EcoBici/Simulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoBici
+{
+    public class Simulation
+    {
+        internal byte?[,] matriz;
+
+        public Simulation(int amountOfBicycles, int amountOfStations)
+        {
+            matriz = new byte?[amountOfStations, amountOfBicycles];
+
+            //Bicycles distribution stategy:
+
+            // TODO: If needed, improve performance by precomputing
+            Action a = () =>
+            {
+                int remainingBikes = amountOfBicycles;
+                for (int bic = 0; bic < matriz.GetLength(1); ++bic)
+                {
+                    for (int est = 0; est < matriz.GetLength(0); ++est)
+                    {
+                        matriz[est, bic] = 0;
+                        if (--remainingBikes == 0)
+                            return;
+                    }
+                }
+            };
+        }
+        
+        public void Run()
+        {
+
+        }
+    }
+}
