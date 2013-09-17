8641b0b60bcb658ce7f560b80425176529c1d0ee
Martin Mihura <mbmihura@gmail.com>
2013-09-17 17:29:29 -0300

    Creates a .net Charting.Charts and implements drag2resize behavior on graph control.

--- EcoBici/Form1.Designer.cs
+++ EcoBici/Form1.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(388, 220);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Enter += new System.EventHandler(this.chart1_Enter);
+            this.chart1.Leave += new System.EventHandler(this.chart1_Leave);
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(10, 10);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseDown);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 10);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 476);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- EcoBici/Form1.cs
+++ EcoBici/Form1.cs
@@ -12,9 +12,97 @@ namespace EcoBici
 {
     public partial class Form1 : Form
     {
+
+        bool mouseClicked = false;
+
+        private void chart1_MouseDown(object sender,
+                     System.Windows.Forms.MouseEventArgs e)
+        {
+            mouseClicked = true;
+        }
+
+        private void chart1_MouseUp(object sender,
+                System.Windows.Forms.MouseEventArgs e)
+        {
+            mouseClicked = false;
+        }
+
+        private void chart1_MouseMove(object sender,
+             System.Windows.Forms.MouseEventArgs e)
+        {
+            if (mouseClicked)
+            {
+                this.chart1.Height = this.button1.Top = this.button1.Top + e.Y;
+                this.chart1.Width = this.button1.Left = this.button1.Left + e.X;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            chart1.Series.Clear();
+            var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
+            {
+                Name = "Series1",
+                Color = System.Drawing.Color.Green,
+                IsVisibleInLegend = false,
+                IsXValueIndexed = true,
+                ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+            };
+
+            this.chart1.Series.Add(series1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                series1.Points.AddXY(i, f(i));
+            }
+            chart1.Invalidate();
+            chart1.SaveImage("C:\\mypic.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+        private double f(int i)
+        {
+            var f1 = 59894 - (8128 * i) + (262 * i * i) - (1.6 * i * i * i);
+            return f1;
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void chart1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chart1_Leave(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            button1.Visible = button2.Visible = true;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            button1.Visible = button2.Visible = false;
         }
     }
 }
