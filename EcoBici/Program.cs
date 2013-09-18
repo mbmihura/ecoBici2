cbebc8d40ef00a37b7e0d66e45504dca7fa0a434
Martin Mihura <mbmihura@gmail.com>
2013-09-18 18:48:39 -0300

    App initially displays console to config setting and run simulation, then displays GUI to show results. Creates NativeMethods to handle console through kernel32.dll

--- EcoBici/Program.cs
+++ EcoBici/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +15,71 @@ namespace EcoBici
         [STAThread]
         static void Main()
         {
+            // Allocate Console
+            NativeMethods.AllocConsole();
+            Console.WriteLine("Buenos Aires Ciudad - Plan de Movilidad Sustentable:");
+            Console.WriteLine("EcoBici BA System Simulation");
+            Console.WriteLine();
+
+            // Load simulation profiles defaults from file
+            Console.Write("Loading simulation profile... ");
+            Console.WriteLine("OK");
+
+            // Ask for a value for profile's not settted variable
+            Console.Write("Amount of bicycles: ");
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            // Create simulation
+            Console.Write("Creating simulation...");
+            var Ti = new TimeSpan(0);
+            var Tf = new TimeSpan(8, 0, 0);
+            Console.WriteLine("OK");
+            Console.WriteLine("");
+
+            // Run simulation
+            Console.Write("Running simulation...");
+            new Simulation(10, 5, new UniformDistribution(), Tf, Ti).Run();
+            Console.WriteLine("OK");
+
+            // load GUI and Results
+            Console.Write("Loading GUI and Results...");
+
+            // Deallocate Console
+            Console.Write("Exiting command interface...");
+            NativeMethods.FreeConsole();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
+
+
+        internal static class NativeMethods
+        {
+            // http://msdn.microsoft.com/en-us/library/ms681944(VS.85).aspx
+            /// <summary>
+            /// Allocates a new console for the calling process.
+            /// </summary>
+            /// <returns>nonzero if the function succeeds; otherwise, zero.</returns>
+            /// <remarks>
+            /// A process can be associated with only one console,
+            /// so the function fails if the calling process already has a console.
+            /// </remarks>
+            [DllImport("kernel32.dll", SetLastError = true)]
+            internal static extern int AllocConsole();
+
+            // http://msdn.microsoft.com/en-us/library/ms683150(VS.85).aspx
+            /// <summary>
+            /// Detaches the calling process from its console.
+            /// </summary>
+            /// <returns>nonzero if the function succeeds; otherwise, zero.</returns>
+            /// <remarks>
+            /// If the calling process is not already attached to a console,
+            /// the error code returned is ERROR_INVALID_PARAMETER (87).
+            /// </remarks>
+            [DllImport("kernel32.dll", SetLastError = true)]
+            internal static extern int FreeConsole();
+        }
 }
