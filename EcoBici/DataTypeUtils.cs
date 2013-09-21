d0f9fb370501499f6d9a44975a39cdaf7dc1689f
Martin Mihura <mbmihura@gmail.com>
2013-09-21 19:57:45 -0300

    Adds Log to output current running simulation to console, and DataTypeUtils to group functions related to it.

---
+++ EcoBici/DataTypeUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoBici
+{
+    static class DataTypeUtils
+    {
+        public static TimeSpan[][] CopyArray(TimeSpan[][] source)
+        {
+            TimeSpan[][] destination = new TimeSpan[source.Length][];
+            Array.Copy(source, destination, source.Length);
+            return destination;
+        }
+    }
+}
