708e59d84b70472a1b8864999c4dcc74a752f398
Martin Mihura <mbmihura@gmail.com>
2013-09-22 11:51:59 -0300

    Creates GenerateFDPs SP to precompuete IA, ED and TV Tables. Adds SP to load trips' data from cvs file.

---
+++ TripsDataWarehouse/EcoBici/Stored Procedures/LoadTripsFromCSVFile.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [EcoBici].[LoadTripsFromCSVFile]
+AS
+	DELETE [EcoBici].[Trips]
+
+	BULK INSERT [EcoBici].[Trips]
+	FROM 'c:\recorrido-bicis-2012.csv' --TODO: set using sp params
+	WITH
+	(
+		FIRSTROW = 1,
+		FIELDTERMINATOR = ',',
+		ROWTERMINATOR = '\n'
+	)
+RETURN 0
