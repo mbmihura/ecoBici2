1684004b14a427625f5e634b14e01b06566dc6b5
Martin Mihura <mbmihura@gmail.com>
2013-09-22 18:22:09 -0300

    Implements DataWarehouseManager to handle database communication.

---
+++ EcoBici/DataWarehouseManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EcoBici
+{
+    class DataWarehouseManager : IDisposable
+    {
+        private SqlConnection connection; 
+        private const string dbSchemaName = "EcoBici";
+
+        public DataWarehouseManager()
+        {
+            connection = new SqlConnection(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"]);
+            connection.Open();
+        }
+
+        public int ExecuteIntFunction(string functionName, params SqlParameter[] parameters) {
+            using (SqlCommand cmd = new SqlCommand(dbSchemaName + "." + functionName, connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter result = new SqlParameter("@retorno", SqlDbType.Int);
+                result.Direction = ParameterDirection.ReturnValue;
+                cmd.Parameters.Add(result);
+
+                foreach (SqlParameter p in parameters)
+                    cmd.Parameters.Add(p).Direction = ParameterDirection.Input;
+
+                cmd.ExecuteNonQuery();
+                return (int)result.Value;
+            }
+        }
+
+        public int ExcuteIA(int estationId)
+        {
+            return ExecuteIntFunction("IA", new SqlParameter("@IdEstacion", estationId));
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+        public virtual void Dispose()
+        {
+            if (connection != null)
+            {
+                Close();
+                connection.Dispose();
+            }
+        }
+    }
+}
