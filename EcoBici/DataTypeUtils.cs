7aa6e0f0a12fd61a72b0da5a230c97b360569c56
Martin Mihura <mbmihura@gmail.com>
2013-09-22 19:20:32 -0300

    Adds doc to DataTypesUtil.

--- EcoBici/DataTypeUtils.cs
+++ EcoBici/DataTypeUtils.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace EcoBici
 {
+    /// <summary>
+    /// Contains methods to perfom general operation among different data types, such as search in an array or copyng it.
+    /// </summary>
     static class DataTypeUtils
     {
+        /// <summary>
+        /// Creates a copy of a given jagged TimeSpan array.
+        /// </summary>
+        /// <param name="source">Jagged array to be copied.</param>
+        /// <returns>New array.</returns>
         public static TimeSpan[][] CopyArray(TimeSpan[][] source)
         {
             TimeSpan[][] destination = new TimeSpan[source.Length][];
