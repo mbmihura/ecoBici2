9bcaa8d571e65fd586dcc83117373b0c91f3b255
Martin Mihura <mbmihura@gmail.com>
2013-09-23 14:32:23 -0300

    Removes unused chart's drag&resize behavior.

--- EcoBici/Form1.cs
+++ EcoBici/Form1.cs
@@ -12,31 +12,6 @@ namespace EcoBici
 {
     public partial class Form1 : Form
     {
-
-        bool mouseClicked = false;
-
-        private void chart1_MouseDown(object sender,
-                     System.Windows.Forms.MouseEventArgs e)
-        {
-            mouseClicked = true;
-        }
-
-        private void chart1_MouseUp(object sender,
-                System.Windows.Forms.MouseEventArgs e)
-        {
-            mouseClicked = false;
-        }
-
-        private void chart1_MouseMove(object sender,
-             System.Windows.Forms.MouseEventArgs e)
-        {
-            if (mouseClicked)
-            {
-                this.chart1.Height = this.button1.Top = this.button1.Top + e.Y;
-                this.chart1.Width = this.button1.Left = this.button1.Left + e.X;
-            }
-        }
-
         public Form1()
         {
             InitializeComponent();
@@ -70,39 +45,6 @@ namespace EcoBici
             return f1;
         }
 
-        private void button2_MouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button2_MouseUp(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void button2_MouseDown(object sender, MouseEventArgs e)
-        {
-
-        }
-
-        private void chart1_Enter(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void chart1_Leave(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void chart1_MouseClick(object sender, MouseEventArgs e)
-        {
-            button1.Visible = button2.Visible = true;
-        }
-
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
-        {
-            button1.Visible = button2.Visible = false;
-        }
+   
     }
 }
