46e50a4352619372b109a108e7aa7e73c5fbfca8
Martin Mihura <mbmihura@gmail.com>
2013-09-25 21:21:35 -0300

    Adds SP LimppiarViajjesErroneos to delete extremely long trips which would compromise simulation's results.

---
+++ TripsDataWarehouse/EcoBici/Stored Procedures/LimpiarViajesErroneos.sql
@@ -0,0 +1,8 @@
+﻿Create PROCEDURE [EcoBici].[LimpiarViajesErroneos]
+AS
+BEGIN
+	-- Elimina viajes que duren mas de 480 minutos, ya que si bien pueden suceder distorcionarian los resultados como el % de uso de bicis del sistema que es lo que realmente interesa obtener.
+	DELETE [TripsDataWarehouse].[EcoBici].[TVTable]
+	WHERE TiempoDeViaje > 480	
+	
+END;
